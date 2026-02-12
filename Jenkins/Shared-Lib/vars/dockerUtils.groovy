ff96e9ce245719a7089b2afb90f2461241cc4496
Noura Abdelnabi <noura.abdelnabi.hassan@gmail.com>
2026-02-12 15:36:12 +0200

    Create dockerUtils.groovy

---
+++ Jenkins/Shared-Lib/vars/dockerUtils.groovy
@@ -0,0 +1,18 @@
+def build(imageName) {
+    sh "docker build -t ${imageName} ."
+}
+
+def scan(imageName) {
+    sh "trivy image --severity HIGH,CRITICAL ${imageName} || echo 'Scan failed but continuing...'"
+}
+
+def push(imageName, credsId) {
+    withCredentials([usernamePassword(credentialsId: credsId, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo \$PASS | docker login -u \$USER --password-stdin"
+        sh "docker push ${imageName}"
+    }
+}
+
+def clean(imageName) {
+    sh "docker rmi ${imageName}"
+}
